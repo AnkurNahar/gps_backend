8530aec849e6ee109ac842a0f497dcc9f237eac9
AnkurNahar <ankurnahar.an@gmail.com>
2024-11-28 11:08:32 -0800

    rename controllers

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import(
 
 	middleware "gps_backend/middleware"
 	storage "gps_backend/storage"
-	handlers "gps_backend/handlers"
+	controllers "gps_backend/controllers"
 )
 
 func main() {
@@ -15,15 +15,15 @@ func main() {
 	
     router := mux.NewRouter()
 
-	router.HandleFunc("/login", handlers.Login).Methods("GET")
+	router.HandleFunc("/login", controllers.Login).Methods("GET")
 
 
 	// Protected routes
 	protected := router.PathPrefix("/").Subrouter()
 	protected.Use(middleware.CheckAuthToken)
-	protected.HandleFunc("/devices", handlers.GetDevices).Methods("GET")
-	protected.HandleFunc("/preferences", handlers.GetPreferences).Methods("GET")
-	protected.HandleFunc("/preferences", handlers.UpdatePreferences).Methods("POST")
+	protected.HandleFunc("/devices", controllers.GetDevices).Methods("GET")
+	protected.HandleFunc("/preferences", controllers.GetPreferences).Methods("GET")
+	protected.HandleFunc("/preferences", controllers.UpdatePreferences).Methods("POST")
 
 	log.Println("Server is running on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
